2a367e40b1befc03865a9403657d292b0caf50c4
cdude <cdude996@gmail.com>
2018-05-13 23:05:14 -0400

    change font size

--- suckless/tabbed/config.h
+++ suckless/tabbed/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "Terminus (TTF):pixelsize=20";
+static const char font[]        = "Terminus (TTF):pixelsize=24";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
